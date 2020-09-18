78aef9fa92d63e7d2533f606036e03dcc8eb788d
japhethca <chidiebere.anyigor@gmail.com>
2020-09-18 20:18:19 +0100

    added server deps

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-const port = process.env.PORT || 9000
+
+const port = process.env.PORT || 9000;
 app.listen(port);
