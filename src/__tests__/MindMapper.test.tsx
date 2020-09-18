18f12642dea1d76c1afd889f70788ab01ac4ac60
japhethca <chidiebere.anyigor@gmail.com>
2020-09-18 20:16:32 +0100

    add test files

---
+++ src/__tests__/MindMapper.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import MindMapper from "../MindMapper";
+
+describe("MindMapper", () => {
+  test("should render successfully", async () => {
+    // const { findByText } = render(<MindMapper />);
+    // await findByText(/start mapping/i);
+  });
+});
